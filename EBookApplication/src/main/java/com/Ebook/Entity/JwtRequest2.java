9f11ad6c8b869c918a12ae96f5899332c7f470d0
niteshpatond <nitesh.patond96@gmail.com>
2022-09-05 11:33:43 +0530

    first commit

---
+++ EBookApplication/src/main/java/com/Ebook/Entity/JwtRequest2.java
@@ -0,0 +1,14 @@
+package com.Ebook.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequest2 {
+	String username;
+	String password;
+	
+}
